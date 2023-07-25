aba028bea555a2d6aacf6f17fc549284d13c3735
Jeff G <jeffg@jeffs-mbp.mynetworksettings.com>
2023-07-25 17:50:10 -0400

    completed level one

--- starter/level-one.js
+++ starter/level-one.js
@@ -2,3 +2,18 @@
  * DOTS: Level One
  *
  */
+let score = 0;
+
+const ball = document.querySelector(".js-ball");
+const scoreDisplay = document.querySelector(".js-score")
+const levelWinner = document.querySelector(".level-winner");
+
+ball.addEventListener('click', function() {
+    if (scoreDisplay.innerText < 90) {
+    scoreDisplay.innerText = score += 10;
+    } else{
+        scoreDisplay.innerText = "100";
+        document.querySelector(".level-winner").style.opacity=100;
+    }
+    console.log("Ball Clicked");
+});
